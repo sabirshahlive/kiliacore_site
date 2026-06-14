e8cca276b011247578c4df36e3fd46cff59e7bc1
Sabir Shah <sabir.shah@live.com>
2026-06-14 17:42:42 +0500

    Favicon redirect

--- kiliacore/urls.py
+++ kiliacore/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, include
+from django.views.generic import RedirectView
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -6,6 +7,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', views.LandingView.as_view(), name='landing'),
     path('contact/', views.ContactFormView.as_view(), name='contact_form'),
+    path('favicon.ico', RedirectView.as_view(url='/static/favicon/favicon.ico', permanent=True)),
 ]
 
 if settings.DEBUG:
